0f61f3eeae5a83436ff9d9a47389833367ea6ace
Victor Kamei Kay <kameikay@gmail.com>
2022-11-02 22:49:50 -0300

    first commit

---
+++ client/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 6*time.Second)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost:8080", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+	io.Copy(os.Stdout, res.Body)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kameikay/client-server_example.go
+
+go 1.19
